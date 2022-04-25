d786d6a35928e15a781714ed930fd96b87670c39
maksudahan <maksuda.2289@gmail.com>
2022-04-25 16:13:02 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       String[] arr = {"maksat", "ayperi","altyn" };
+          for ( String a : arr){
+              System.out.println(a + " men seni jakshy korom ");
+          }
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
